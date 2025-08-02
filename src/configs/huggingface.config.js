24a97e7d2d71522bba5497f508b1195179fc07f1
Clement-Zhang <zhangclement947@yahoo.ca>
2025-08-02 00:44:53 -0400

    shorten some code

--- src/configs/huggingface.config.js
+++ src/configs/huggingface.config.js
@@ -1,6 +1,5 @@
 const { InferenceClient } = require('@huggingface/inference');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 const api = new InferenceClient(process.env.HUGGINGFACE_KEY);
 
 module.exports = api;
