5afc7b4818d1e303660562f79e271ca48038a0de
Clement-Zhang <zhangclement947@yahoo.ca>
2025-09-30 22:18:18 -0400

    backend will only be run from render

--- src/middleware/cors.middleware.js
+++ src/middleware/cors.middleware.js
@@ -1,15 +1,8 @@
 import 'dotenv/config';
 
-const urls = process.env.CORS_ORIGIN_PRODUCTION.split(',');
-
 export default {
     origin: (origin, callback) => {
-        if (
-            !origin ||
-            (process.env.NODE_ENV === 'development' &&
-                origin === process.env.CORS_ORIGIN_DEVELOPMENT) ||
-            (process.env.NODE_ENV === 'production' && urls.includes(origin))
-        ) {
+        if (!origin || process.env.CORS_ORIGINS.split(',').includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error('Blocked by CORS'));
