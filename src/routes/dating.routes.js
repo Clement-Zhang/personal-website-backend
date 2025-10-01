8d91a0a516b1aa2073ce8b51bf02e1cb480ed12b
Clement-Zhang <zhangclement947@yahoo.ca>
2025-09-30 21:50:34 -0400

    test

--- src/routes/dating.routes.js
+++ src/routes/dating.routes.js
@@ -2,6 +2,10 @@ import { reset } from '../controllers/dating.controller.js';
 import { Router } from 'express';
 
 const dating = Router();
+
 dating.post('/reset', reset);
+dating.get('/wake', (_, res) => {
+    res.end();
+});
 
 export default dating;
