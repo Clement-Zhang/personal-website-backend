5d068afba63006a2585831246ce499ceb5dbe1af
Clement-Zhang <zhangclement947@yahoo.ca>
2025-09-30 21:53:13 -0400

    change cors handling

--- src/middleware/cors.middleware.js
+++ src/middleware/cors.middleware.js
@@ -1,13 +1,14 @@
 import 'dotenv/config';
 
+const urls = process.env.CORS_ORIGIN_PRODUCTION.split(',');
+
 export default {
     origin: (origin, callback) => {
         if (
             !origin ||
             (process.env.NODE_ENV === 'development' &&
                 origin === process.env.CORS_ORIGIN_DEVELOPMENT) ||
-            (process.env.NODE_ENV === 'production' &&
-                origin === process.env.CORS_ORIGIN_PRODUCTION)
+            (process.env.NODE_ENV === 'production' && urls.includes(origin))
         ) {
             callback(null, true);
         } else {
