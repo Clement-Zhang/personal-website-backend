e17fb4e95d418aed02de71f8e2e2d1460bfedb24
Clement-Zhang <zhangclement947@yahoo.ca>
2025-10-01 23:24:07 -0400

    Revert "check cors"
    This reverts commit 719ae51a9fd3fd2c3655ad048c14d3947bda6740.

--- src/middleware/cors.middleware.js
+++ src/middleware/cors.middleware.js
@@ -2,12 +2,6 @@ import 'dotenv/config';
 
 export default {
     origin: (origin, callback) => {
-        console.log(
-            'origin ',
-            origin,
-            'list ',
-            process.env.CORS_ORIGINS.split(',')
-        );
         if (!origin || process.env.CORS_ORIGINS.split(',').includes(origin)) {
             callback(null, true);
         } else {
