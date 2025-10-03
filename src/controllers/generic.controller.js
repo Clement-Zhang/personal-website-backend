7e6859120c92712b9655f759661eee1793b90a99
Clement-Zhang <zhangclement947@yahoo.ca>
2025-10-02 22:38:03 -0400

    try it without pinging stuff this time

--- src/controllers/generic.controller.js
+++ src/controllers/generic.controller.js
@@ -1,6 +1,3 @@
-import { ping } from '../services/generic.service.js';
-
 export async function wake(_, res) {
-    await ping();
-    res.send("done");
+    res.end();
 }
--- src/services/generic.service.js
+++
@@ -1,7 +0,0 @@
-import { collection } from '../configs/mongo.config.js';
-import { summarizeProfile } from './dating.service.js';
-
-export async function ping() {
-    await collection.findOne();
-    await summarizeProfile('');
-}
