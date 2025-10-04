b77afcc0b586968ca4dc06733bb2e86312a5f2d2
Clement-Zhang <zhangclement947@yahoo.ca>
2025-10-04 01:12:38 -0400

    Update cors.middleware.js

--- src/middleware/cors.middleware.js
+++ src/middleware/cors.middleware.js
@@ -2,9 +2,7 @@ import 'dotenv/config';
 
 export default {
     origin: (origin, callback) => {
-        console.log('origin', origin);
         if (!origin || process.env.CORS_ORIGINS.split(',').includes(origin)) {
-            console.log('allowed by CORS');
             callback(null, true);
         } else {
             callback(new Error('Blocked by CORS'));
