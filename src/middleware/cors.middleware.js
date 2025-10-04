2001d3baa7b79f61563f083cbd423c422d0a7ce3
Clement-Zhang <zhangclement947@yahoo.ca>
2025-10-04 00:14:57 -0400

    Update cors.middleware.js

--- src/middleware/cors.middleware.js
+++ src/middleware/cors.middleware.js
@@ -4,6 +4,7 @@ export default {
     origin: (origin, callback) => {
         console.log('origin', origin);
         if (!origin || process.env.CORS_ORIGINS.split(',').includes(origin)) {
+            console.log('allowed by CORS');
             callback(null, true);
         } else {
             callback(new Error('Blocked by CORS'));
