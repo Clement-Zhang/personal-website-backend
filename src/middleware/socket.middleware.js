f2a10cfb74549d902889418256c47c38bb57bc73
Clement-Zhang <zhangclement947@yahoo.ca>
2025-10-06 16:54:09 -0400

    switch devices

--- src/middleware/socket.middleware.js
+++ src/middleware/socket.middleware.js
@@ -1,8 +1,12 @@
 import { reformat, match } from '../controllers/dating.controller.js';
 
 export default (socket) => {
-    socket.on('reformat', async (data, callback) => {
-        callback(await reformat(data));
+    socket.on('reformat', async (data) => {
+        const stream = await reformat(data);
+        socket.emit('start reformat');
+        for await (const chunk of stream) {
+            socket.emit('reformatting', chunk.choices[0].delta.content);
+        }
     });
 
     socket.on('match', async (data, callback) => {
