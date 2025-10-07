a8875634dd8d9aca461956fa88e9fdfc2b73d356
Clement-Zhang <zhangclement947@yahoo.ca>
2025-10-06 22:22:43 -0400

    reformat emits

--- src/controllers/socket.controller.js
+++ src/controllers/socket.controller.js
@@ -4,7 +4,17 @@ import {
     summarizeProfile,
 } from '../services/dating.service.js';
 
-export const reformat = async (prompt) => await reformatProfile(prompt);
+export const reformat = async (prompt, socket) => {
+    const stream = await reformatProfile(prompt);
+    for await (const chunk of stream) {
+        socket.emit('reformat', {
+            type: 'res',
+            chunk: chunk.choices[0].delta.content,
+        });
+    }
+    socket.emit('reformat', { type: 'end' });
+};
 
-export const match = async (profile) =>
-    await getMatches(await summarizeProfile(profile));
+export const match = async (profile, callback) => {
+    callback(await getMatches(await summarizeProfile(profile)));
+};
--- src/middleware/socket.middleware.js
+++ src/middleware/socket.middleware.js
@@ -1,15 +1,8 @@
 import { reformat, match } from '../controllers/socket.controller.js';
 
 export default (socket) => {
-    socket.on('reformat', async (data) => {
-        const stream = await reformat(data);
-        socket.emit('start reformat');
-        for await (const chunk of stream) {
-            socket.emit('reformatting', chunk.choices[0].delta.content);
-        }
-    });
-
-    socket.on('match', async (data, callback) => {
-        callback(await match(data));
-    });
+    socket.on('reformat', async (data) =>
+        data.type === 'req' ? reformat(data.prompt, socket) : null
+    );
+    socket.on('match', async (data, callback) => match(data, callback));
 };
