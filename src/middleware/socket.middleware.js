02f122aca73028a955cd20273406c6219a62f45e
Clement-Zhang <zhangclement947@yahoo.ca>
2025-10-09 00:02:57 -0400

    Update socket.middleware.js

--- src/middleware/socket.middleware.js
+++ src/middleware/socket.middleware.js
@@ -2,7 +2,9 @@ import { reformat, match } from '../controllers/socket.controller.js';
 
 export default (socket) => {
     socket.on('reformat', async (data) =>
-        data.type === 'req' ? reformat(data.prompt, socket) : null
+        data.type === 'req'
+            ? reformat(data.prompt, socket)
+            : console.log(data.type)
     );
     socket.on('match', async (data, callback) => match(data, callback));
 };
