645213def5a3764b803b562ca547856b12223184
Clement-Zhang <zhangclement947@yahoo.ca>
2026-02-26 19:38:59 -0500

    Update analytics.socket.js

--- src/middleware/sockets/analytics.socket.js
+++ src/middleware/sockets/analytics.socket.js
@@ -2,5 +2,9 @@ import { update } from '../../controllers/sockets/analytics.socket.js';
 import { changes } from '../../configs/mongo.config.js';
 
 export default (socket) => {
-    changes.on('change', async () => await update(socket));
+    changes.on('change', async () => {
+        console.log('Change detected, emitting update');
+        await update(socket);
+        console.log('Update emitted');
+    });
 };
